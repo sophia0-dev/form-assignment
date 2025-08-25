3d56509624e38a27aa33c711f35089f3e5b3820b
sophia0-dev <sophiaononogbu@gmail.com>
2025-08-25 13:33:55 +0100

    first commit

---
+++ form.js
@@ -0,0 +1,63 @@
+let body = document.getElementById("body");
+let title = document.getElementById("title");
+let input = document.getElementById("e-mail");
+let input1 = document.getElementById("password");
+let input2 = document.getElementById("repeat");
+let check = document.getElementsByClassName("check");
+let sign = document.getElementById("Sign");
+let signs = document.getElementsByClassName("Sign");
+// document.getElementsByTagName("hr").style.backgroundColor = "grey";
+sign.style.color = "yellow";
+// signs.style.color = "yellow";
+// document.getElementsByTagName("img").style.color = "white";
+
+
+let btn = document.getElementById("btn");
+btn.style.width ="100%";
+btn.style.height = "30px";
+btn.style.color = "yellow";
+btn.style.fontSize = "15px"
+btn.style.textAlign = "center"
+btn.style.padding = "20px";
+btn.style. borderRadius = "15px";
+btn.style.backgroundColor = "grey";
+
+body.style.backgroundColor = "black";
+body.style.color = "white";
+body.style.padding = "40px 40px 60px 40px";
+
+title.style.fontFamily = "helvetica";
+title.style.font = "40px";
+
+input.style.backgroundColor = "black";
+input.style.padding = "10px";
+input.style.border = "none";
+
+
+input1.style.backgroundColor = "transparent";
+input1.style.padding = "10px";
+input1.style.border = "none";
+
+
+input2.style.backgroundColor = "transparent";
+input2.style.padding = "10px";
+input2.style.border = "none";
+
+check.style.display = "flex";
+check.style.gap = "50px";
+
+document.getElementById("days").style.backgroundColor = "grey";
+
+// document.querySelector("#btn").style.backgroundColor = "grey";
+ 
+
+
+
+
+
+
+
+// document.getElementsByTagName("input").style.backgroundColor = "black";
+
+
+// document.getElementById("body").style.color = "black";
